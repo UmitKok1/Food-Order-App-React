cd9577dad8a0660b93ce25e9b6a7ae682a2b93c9
UmitKok21 <umit.kok21@gmail.com>
2025-04-13 19:39:30 +0300

    Opening the Cart in the modal via a New Context.

---
+++ src/components/Cart.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react";
+import Modal from "./UI/Modal";
+import CartContext from "../store/CartContext";
+import { currencyFormatter } from "../util/formatting";
+import UserProgressContext from "../store/UserProgressContext";
+import Button from "./UI/Button";
+
+export default function Cart() {
+    const cartCtx = useContext(CartContext);
+    const userProgresCtx = useContext(UserProgressContext);
+    const cartTotal = cartCtx.items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0);
+
+    function handleCloseCart() {
+        userProgresCtx.hideCart();
+    }
+
+    return <Modal className="cart" open={userProgresCtx.progress === 'cart'}>
+        <h2>Your cart</h2>
+        <ul>
+            {cartCtx.items.map(item => (
+                <li key={item.id}>
+                    {item.name} - {item.quantity}
+                </li>
+            ))}
+        </ul>
+        <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
+        <p className="modal-actions">
+            <Button textOnly onClick={handleCloseCart}>
+                Close
+            </Button>
+            <Button textOnly onClick={handleCloseCart}>
+                Go to Checkout
+            </Button>
+        </p>
+    </Modal>
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,29 @@ import { useContext } from 'react';
 import Button from './UI/Button';
 import logoImg from '../assets/logo.jpg';
 import CartContext from '../store/CartContext';
+import UserProgressContext from '../store/UserProgressContext';
 
 export default function Header() {
     const cartCtx = useContext(CartContext);
-
+    const userProgresCtx = useContext(UserProgressContext);
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
         return totalNumberOfItems + item.quantity;
     }, 0);
+
+    function handleShowCart() {
+        userProgresCtx.showCart();
+        console.log(userProgresCtx.progress)
+    }
+
     return <header id="main-header">
         <div id="title">
             <img src={logoImg} alt='' />
             <h1>Food App</h1>
         </div>
         <nav>
-            <Button textOnly>Cart ({totalCartItems})</Button>
+            <Button textOnly onClick={handleShowCart}>
+                Cart ({totalCartItems})
+            </Button>
         </nav>
     </header>
 }
